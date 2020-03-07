e467e5f9d6cdb4f4ec8a05a0dfa6287a75cb1736
unknown <vensoneandrew@gmail.com>
2020-03-07 02:26:45 -0600

    footer styling

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,24 +1,24 @@
-<div class='footer-nav row' style='text-align:center; position:fixed; width:100%; bottom:0;height:50px; color:white;'>
+<div class='footer-nav row' style='text-align:center; position:fixed; width:100%; bottom:10;height:50px; color:white;'>
     <div class="col" style='margin-top:10px;'>
         @if(Request::path() == "feed")
-            <a href="{{url('/feed')}}"><img src="images/feedfl.png" style="width:24px; height:24px;"alt=""> </a>
+            <a href="{{url('/feed')}}"><img src="images/feedfl.png" style="width:20px; height:20px;"alt=""> </a>
         @else
-            <a href="{{url('/feed')}}"><img src="images/feedol.png" style="width:24px; height:24px;"alt=""> </a>
+            <a href="{{url('/feed')}}"><img src="images/feedol.png" style="width:20px; height:20px;"alt=""> </a>
         @endif
     </div>
     <div class="col" style='margin-top:10px;'>
         @if(Request::path() == "/" || Request::path() == "fits" )
-            <a href="{{url('/')}}"><img src="images/shirtfl.png" style='width:24px; height:24px' alt=""></a>
+            <a href="{{url('/')}}"><img src="images/shirtfl.png" style='width:20px; height:20px' alt=""></a>
         @else
-            <a href="{{url('/')}}"><img src="images/shirtol.png" style='width:24px; height:24px' alt=""></a>
+            <a href="{{url('/')}}"><img src="images/shirtol.png" style='width:20px; height:20px' alt=""></a>
         @endif
     </div>
     <div class="col" style='margin-top:10px;'>
 
         @if(Request::path() == "profile")
-            <a href="{{url('/profile')}}"><img src="images/userfl.png"  style='width:24px; height:24px' alt=""></a>
+            <a href="{{url('/profile')}}"><img src="images/userfl.png"  style='width:20px; height:20px' alt=""></a>
         @else       
-            <a href="{{url('/profile')}}"><img src="images/userol.png"  style='width:24px; height:24px' alt=""></a>
+            <a href="{{url('/profile')}}"><img src="images/userol.png"  style='width:20px; height:20px' alt=""></a>
         @endif
         
     </div>

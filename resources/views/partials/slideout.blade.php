a76570d3d8e9ff09990148d76fda3afa583610ef
unknown <vensoneandrew@gmail.com>
2020-03-08 18:16:10 -0500

    .

--- resources/views/partials/slideout.blade.php
+++ resources/views/partials/slideout.blade.php
@@ -6,9 +6,9 @@
 <div class='closet-so' style='position:fixed;opacity: .8;display:none;  right: -40px; top:50%;box-shadow: 2px 3px 4px black;z-index: 9999999;  background-color:#1c1b17; height: 100px; width: 100%; border-radius: 50px;'>
     <img src="/images/popright.png" alt="" style='width: 25px; height: 25px;position:fixed;right:0;margin-top:38px;'>
     <div style="position:fixed;right: 65px;margin-top:25px;">
-        <a href="{{url('/newitem')}}"><img src="images/add.png" style='margin-left:10px; width: 50px; height: 50px;' alt=""></a>
+        <a href="{{url('/newitem')}}"><img src="images/addshirt.png" style='margin-left:10px; width: 50px; height: 50px;' alt=""></a>
 
-        <a class='fit' ><img src="images/addshirt.png" style='width: 50px; height:50px; margin-left:10px' alt=""></a>
+        <a class='fit' ><img src="images/pant.png" style='width: 50px; height:50px; margin-left:10px' alt=""></a>
         <a class='filter' data-toggle="modal" data-target="#filter"><img src="images/filter.png" style='width: 45px; height:45px; margin-left:10px' alt=""></a>
         
         <img src="images/four.png" style='margin-left:10px;width: 45px; height:45px'>

8fcccccc949db680adf500e9827f03f7d49892e7
unknown <vensoneandrew@gmail.com>
2020-03-08 20:10:19 -0500

    .

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<div class='footer-nav row' style='text-align:center; position:fixed; width:100%; bottom:0px;height:75px; background-color: #121212; color:white;z-index:99999999; opacity:.9;'>
+<div class='footer-nav row' style='text-align:center; position:fixed; width:100%; bottom:0px;height:50px; background-color: #121212; color:white;z-index:99999999; opacity:.9;'>
     <div class="col" style='margin-top:10px;'>
         @if(Request::path() == "feed")
             <a href="{{url('/feed')}}"><img src="images/feedfl.png" style="width:24px; height:24px;"alt=""> </a>
